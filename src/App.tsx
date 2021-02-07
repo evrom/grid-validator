ae9760188851ad4e449832789e773784a236f168
Evan Roman <evanroman1@gmail.com>
2021-02-07 15:06:01 -0800

    add header and default value to show

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,20 @@ import GridPreview from './GridPreview';
 import CssGridTemplateAreas from './CssGridTemplateAreas'
 
 function App() {
-  const [gridTemplateAreas, setGridTemplateAreas] = useState("");
+  const [gridTemplateAreasInput, setGridTemplateAreas] = useState(`"a a ."
+"a a ."
+". b c";`);
 
   return (
     <div className="App">
-      <textarea onChange={e => setGridTemplateAreas(e.target.value)} value={gridTemplateAreas}></textarea>
-    <GridPreview cssGridTemplateAreas={new CssGridTemplateAreas(gridTemplateAreas)}></GridPreview>
+      <header>
+        <h1>CSS Grid Template Validator & Preview</h1>
+        <p>Paste your grid <code>grid-template-areas</code> property value and see a preview and diagnosis what is wrong.</p>
+      </header>
+      <main>
+      <textarea onChange={e => setGridTemplateAreas(e.target.value)} value={gridTemplateAreasInput}></textarea>
+      <GridPreview cssGridTemplateAreas={new CssGridTemplateAreas(gridTemplateAreasInput)}></GridPreview>
+      </main>
     </div>
   );
 }
