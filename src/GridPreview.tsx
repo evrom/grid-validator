d2030ae7902bcb6594c44ed6f513c28de0545c28
Evan Roman <evanroman1@gmail.com>
2021-02-09 00:18:10 -0800

    add and remove grid children

--- src/GridPreview.tsx
+++ src/GridPreview.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 type GridPreviewProps = {
   propertyValue: string,
   namedAreas: Set<string>
 }
 
-function GridPreview({propertyValue, namedAreas}: GridPreviewProps) {
+function GridPreview({ propertyValue, namedAreas }: GridPreviewProps) {
   const gridStyleNamedRegions = {
     gridTemplateAreas: propertyValue
   }
 
+  const [count, setCount] = useState(0)
+
   return (
-      <>
-        <h2>View preview of named areas</h2>
-        <p>Rendered using CSS grid.</p>
+    <>
+      <h2>View preview of named areas</h2>
+      <div>
+        Rendered using CSS grid.
+        <button onClick={() => setCount(count + 1)}>Add grid child</button>
+        {count > 0 ? <button onClick={() => setCount(count - 1)}>Remove grid child</button> : ''}
+      </div>
       <div className="grid-preview" style={gridStyleNamedRegions}>
         {Array.from(namedAreas).map((name: string, index: number) => {
-          return <div key={name} style={{gridArea: `${name}`}}>{name}</div>
+          return <div key={name} style={{ gridArea: `${name}` }}>{name}</div>
         })}
+        {Array.from(Array(count)).map(() => (
+          <div>.</div>
+        ))}
       </div>
-      </>
+    </>
   );
 }
 
